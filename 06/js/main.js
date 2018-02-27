76ee8b64c6c2cde62e957ecb58b57067dfcad5e4
Heesang Chae <theand@gmail.com>
2018-02-27 16:22:24 +0900

    question 5,6 boilerplate

---
+++ 06/js/main.js
@@ -0,0 +1,14 @@
+//0. common
+
+
+
+//3. benchmark
+const timeIt = (n, name, expected, func, args) => {
+    console.group(`${name} : ${n}`);
+
+    console.assert(func(n) === expected, `${name} - expected ${expected}, but actual ${func(n, args)}`);
+
+    console.groupEnd();
+};
+
+
---
+++ 06/js/testcase.js
@@ -0,0 +1,12 @@
+console.log("-----------------------XXX");
+
+let funcNum = XXX;
+
+const input1 = XXX;
+timeIt(input1, Object.keys({input1})[0], XXX, funcNum);
+
+
+const input123 = X;
+timeIt(input123, Object.keys({input123})[0], XXX, funcNum);
+
+let func = isIntegerPalindrome;
