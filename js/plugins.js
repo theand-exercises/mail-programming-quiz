98d7ac065edd36329651dbdf2ece6bc3b23a8c84
Heesang Chae <theand@gmail.com>
2018-03-19 11:21:08 +0900

    add equal predicate to timeit

--- js/plugins.js
+++ js/plugins.js
@@ -22,10 +22,14 @@
 }());
 
 // Place any jQuery/helper plugins in here.
-const timeIt = (n, name, expected, func, args) => {
+const timeIt = (n, name, expected, func, equalFn, args) => {
     console.group(`${name} : ${n}`);
 
-    console.assert(func(n, args) === expected, `${name} - expected ${expected}, but actual ${func(n, args)}`);
+    if( typeof equalFn !== "function" ){
+        equalFn = (a,b) => a===b;
+    }
+
+    console.assert(equalFn( func(n, args) , expected), `${name} - expected [${expected}], but actual [${func(n, args)}]`);
 
     console.groupEnd();
 };
