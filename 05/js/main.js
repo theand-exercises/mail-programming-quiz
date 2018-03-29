dd1b34f6fd78a98a01e62566a5a8f12d47992b72
Heesang Chae <theand@gmail.com>
2018-03-29 18:04:37 +0900

    solve quiz5.

--- 05/js/main.js
+++ 05/js/main.js
@@ -1,1 +1,23 @@
 //0. common
+
+const findIndexOfTargetSum = (obj) => {
+    const target = obj.target;
+
+    const indexByValue = obj.array.reduce((acc, cur, ind) => {
+        acc.set(cur, ind);
+        return acc;
+    }, new Map());
+
+    const res = obj.array.reduce((acc, cur, index) => {
+        const searchForValue = target - cur;
+        if (indexByValue.has(searchForValue) && acc.found === false) {
+            acc.pos.push(index);
+            acc.pos.push(indexByValue.get(searchForValue));
+            acc.found = true;
+        }
+        return acc;
+    }, {pos: [], found: false});
+
+    return res.pos;
+
+};
--- 05/js/testcase.js
+++ 05/js/testcase.js
@@ -1,12 +1,22 @@
 console.log("-----------------------XXX");
 
-let funcNum = XXX;
+let func = findIndexOfTargetSum;
 
-const input1 = XXX;
-timeIt(input1, Object.keys({input1})[0], XXX, funcNum);
+const input1 = {array: [2, 5, 6, 1, 10], target: 8};
+const result1 = [0, 2];
+timeIt(input1, Object.keys({input1})[0], result1, func);
+
+const input2 = {array: [10, 2, 5, 6, 1 ], target: 8};
+const result2 = [1, 3];
+timeIt(input2, Object.keys({input2})[0], result2, func);
 
 
-const input123 = X;
-timeIt(input123, Object.keys({input123})[0], XXX, funcNum);
+const input3 = {array: [10, 1, 2, 5, 6], target: 8};
+const result3 = [2, 4];
+timeIt(input3, Object.keys({input3})[0], result3, func);
 
-let func = isIntegerPalindrome;
+
+const input4 = {array: [10, 6, 1, 2, 5], target: 8};
+const result4 = [1, 3];
+timeIt(input4, Object.keys({input4})[0], result4, func);
+
