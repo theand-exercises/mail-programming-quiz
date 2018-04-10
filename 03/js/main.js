c33c4ee90a0c7118b10cf84bddae6f5afc20df5a
Heesang Chae <theand@gmail.com>
2018-04-10 15:57:11 +0900

    ongoing

--- 03/js/main.js
+++ 03/js/main.js
@@ -1,2 +1,21 @@
 //0. common
 
+const parenPermutation = (n) => {
+    const result = initializeArrayWithRange(n, 1).reduce((acc, cur, arr) => {
+        if (arr.length <= 1) return "()";
+
+        let perm = "";
+        acc.new_stack.push("(");
+        perm = "X";
+        acc.old_stacks.push(acc.new_stack);
+        while (acc.new_stack.length > 0) {
+            acc.new_stack.pop();
+            perm += ")"
+        }
+        acc.permutation.push(perm);
+
+        return acc;
+    }, {new_stack: [], old_stacks: [], permutation: []});
+
+    return result.permutation;
+};
--- 03/js/testcase.js
+++ 03/js/testcase.js
@@ -1,7 +1,18 @@
-console.log("-----------------------Fibo.");
+console.log("-----------------------parenPermutation.");
+
+const func = parenPermutation;
+
+const input1 = 1;
+const result1 = ["()"];
+timeIt(input1, Object.keys({input1})[0], result1, func);
 
 
+const input2 = 2;
+const result2 = ["(())", "()()"];
+timeIt(input2, Object.keys({input2})[0], result2, func);
 
-const ex1 = 12;
-timeIt(ex1, Object.keys({ex1})[0], 10, func);
+
+const input3 = 3;
+const result3 = ["((()))", "(())", "()(())", "(())()", "()()()"];
+timeIt(input3, Object.keys({input3})[0], result3, func);
 
